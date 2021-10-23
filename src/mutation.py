0a82bb0d3fcaecc6c7b9a9fbb291ae94f9053cd8
bkubick <bjk2158@columbia.edu>
2021-10-23 18:44:43 -0400

    20211020: Mutation Module Methods

--- src/mutation.py
+++ src/mutation.py
@@ -2,11 +2,40 @@
 
 import random
 
-import numpy as np
 from typing import List
 
 
 class Mutation:
+
+    @classmethod
+    def reverse_gene_subset_random_size(cls, genes: List):
+        """ Reverses a subset of random size
+            args:
+                genes (List): list of the genes
+        """
+        # Grabbing two random indices
+        subset_indices = cls._select_k_unique_indices(2, max_index=len(genes)-1)
+
+        # Sorting two randomly selected indices
+        subset_indices.sort()
+
+        # Reversing the order of the subset
+        genes[subset_indices[0]:subset_indices[1]+1] = genes[subset_indices[1]:subset_indices[0]-1:-1]
+
+    @classmethod
+    def reverse_gene_subset_exact_size(cls, genes: List, subset_size: int = 2):
+        """ Reverses a subset of size subset_size
+            args:
+                genes (List): list of the genes
+                
+                subset_size (int): length of the subset to be shuffled
+        """
+        # Declare the min and max index for the subset inside genes
+        subset_min_index = random.randint(0, len(genes)-subset_size)
+        subset_max_index = subset_min_index + subset_size - 1
+
+        # Reversing the order of the subset
+        genes[subset_min_index:subset_max_index + 1] = genes[subset_max_index:subset_min_index-1:-1]
 
     @classmethod
     def shuffle_random_gene_subset(cls, genes: List, subset_size: int = 2):
@@ -17,14 +46,14 @@ class Mutation:
                 subset_size (int): length of the subset to be shuffled
         """
         # Declare the min and max index for the subset inside genes
-        subset_min_index = random.randint(0, len(genes)-1-subset_size)
-        subset_max_index = subset_min_index + subset_size
+        subset_min_index = random.randint(0, len(genes)-subset_size)
+        subset_max_index = subset_min_index + subset_size - 1
 
-        # Shuffle the k genes
-        cls.shuffle_k_random_genes(genes[subset_min_index:subset_max_index+1], subset_size)
+        random_gene_indices = cls._select_k_unique_indices(subset_size, subset_max_index, subset_min_index)
+        cls._shuffle_or_shift(genes, random_gene_indices)
     
     @classmethod
-    def shift_k_random_genes(cls, genes: List, k: int = 2, reverse: bool = False):
+    def shift_k_random_genes(cls, genes: List, k: int = 2):
         """ Shifts k randomly selected genes from a list.
             args:
                 genes (List): list of the genes
@@ -33,13 +62,11 @@ class Mutation:
 
                 reverse (bool): direction genes are shifted
         """
-        # Converting Genes to numpy array if necessary
-        genes = np.asarray(genes)
-
         # Initialize k random genes
-        random_gene_indices = cls.select_k_unique_indices(k=k, max_index=len(genes)-1)
+        random_gene_indices = cls._select_k_unique_indices(k=k, max_index=len(genes)-1)
+        random_gene_indices.sort()
 
-        cls.shuffle_or_shift(genes, random_gene_indices, reverse)
+        cls._shuffle_or_shift(genes, random_gene_indices)
 
     @classmethod
     def shuffle_k_random_genes(cls, genes: List, k: int = 2):
@@ -50,44 +77,30 @@ class Mutation:
                 k (int): number of genes to be switched
         """
         # Initialize k random genes
-        random_gene_indices = cls.select_k_unique_indices(k=k, max_index=len(genes)-1)
-        random_gene_indices = np.random.permutation(random_gene_indices)
+        random_gene_indices = cls._select_k_unique_indices(k=k, max_index=len(genes)-1)
 
-        cls.shuffle_or_shift(genes, random_gene_indices)
+        cls._shuffle_or_shift(genes, random_gene_indices)
     
     @classmethod
-    def shuffle_or_shift(cls, genes: List, random_gene_indices: List, reverse: bool = False):
+    def _shuffle_or_shift(cls, genes: List, random_gene_indices: List):
         """ Shuffles k randomly selected genes from a list and returns the new list.
             args:
                 genes (List): list of the genes
                 
                 k (int): number of genes to be switched
-
-                reverse (bool): direction genes are shifted
         """
-        if reverse:
-            # Save last random_gene index gene value
-            first_gene = genes[random_gene_indices[0]]
+        # Save last random_gene index gene value
+        last_gene = genes[random_gene_indices[len(random_gene_indices)-1]]
 
-            # Set random_index[i] gene to random_index[i-1] gene 
-            for i in range(0, len(random_gene_indices)-1):
-                genes[random_gene_indices[i]] = genes[random_gene_indices[i+1]]
+        # Set random_index[i] gene to random_index[i-1] gene 
+        for i in range(len(random_gene_indices)-1, 0, -1):
+            genes[random_gene_indices[i]] = genes[random_gene_indices[i-1]]
 
-            # Set first random_gene index gene value to last_gene value
-            genes[random_gene_indices[len(random_gene_indices)-1]] = first_gene
-        else:
-            # Save last random_gene index gene value
-            last_gene = genes[random_gene_indices[len(random_gene_indices)-1]]
-
-            # Set random_index[i] gene to random_index[i-1] gene 
-            for i in range(len(random_gene_indices)-1, 0, -1):
-                genes[random_gene_indices[i]] = genes[random_gene_indices[i-1]]
-
-            # Set first random_gene index gene value to last_gene value
-            genes[random_gene_indices[0]] = last_gene
+        # Set first random_gene index gene value to last_gene value
+        genes[random_gene_indices[0]] = last_gene
 
     @classmethod
-    def select_k_unique_indices(cls, k: int, max_index: int = 9, min_index: int = 0,) -> List:
+    def _select_k_unique_indices(cls, k: int, max_index: int = 9, min_index: int = 0,) -> List:
         """ Selectes k unique gene indices between the min and max numbers declared
             args:
                 k (int): number of unique genes to be selected
--- src/tests/mutation_test.py
+++ src/tests/mutation_test.py
@@ -1,10 +1,6 @@
 # coding: utf-8
 
-import random
-
 import pytest
-import numpy as np
-from typing import List
 
 from src.mutation import Mutation
 
@@ -13,7 +9,7 @@ def test_select_k_unique_indices():
     max_index = 9
     min_index = 0
 
-    unique_indices = Mutation.select_k_unique_indices(k=k, max_index=max_index, min_index=min_index)
+    unique_indices = Mutation._select_k_unique_indices(k=k, max_index=max_index, min_index=min_index)
 
     assert len(unique_indices) == k
     assert max(unique_indices) <= max_index
@@ -22,13 +18,9 @@ def test_select_k_unique_indices():
 
 def test_shuffle_or_shift():
     genes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    genes_reversed = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     random_indices = [0, 3, 5, 9]
     new_genes = [9, 1, 2, 0, 4, 3, 6, 7, 8, 5]
-    new_genes_reverse = [3, 1, 2, 5, 4, 9, 6, 7, 8, 0]
 
-    Mutation.shuffle_or_shift(genes, random_indices)
-    Mutation.shuffle_or_shift(genes_reversed, random_indices, reverse=True)
+    Mutation._shuffle_or_shift(genes, random_indices)
 
     assert genes == new_genes
-    assert genes_reversed == new_genes_reverse
